14085fb433a74e404ccf4fd01a4595f31665bf77
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-08-14 15:19:31 +0300

    add ipython

--- Makefile
+++ Makefile
@@ -20,3 +20,6 @@ lint:
 
 run:
 	uv run python3 manage.py runserver
+
+shell:
+	uv run python3 manage.py shell_plus --ipython
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
