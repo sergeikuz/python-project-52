eff5df5d25dd483c13a3e93ca1c6d3f166393b03
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-09-08 20:58:59 +0300

    fix selector in task executor

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -68,7 +68,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         super().delete(*args, **kwargs)
 
     def __str__(self):
-        return self.username
+        return f"{self.first_name} {self.last_name}".strip()
     
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
